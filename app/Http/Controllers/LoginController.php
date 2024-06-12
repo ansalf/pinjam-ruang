ab60b29506bb0b36e4c22581163983d0a454f86a
ansalf <anissaalifia6@gmail.com>
2024-06-12 13:48:04 +0700

    coba

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login.index', [
+            'title' => 'Login',
+            'active' => 'login'
+        ]);
+    }
+
+    // public function authenticate(Request $request)
+    // {
+    //     $credentials = $request->validate([
+    //         'email' => 'required|email',
+    //         'password' => 'required'
+    //     ]);
+
+    //     if (Auth::attempt($credentials)) {
+    //         $request->session()->regenerate();
+
+    //         return redirect()->intended('/');
+    //     }
+
+    //     return back()->with('loginError', 'Gagal melakukan proses autentikasi. Mohon untuk mengisi email & password dengan benar.');
+    // }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+    
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+    
+            $user = Auth::user();
+            if ($user->role_id === 1) { 
+                return redirect()->intended('/dashboard/admin');
+            } elseif ($user->role_id === 2) { 
+                return redirect()->intended('/');
+            }
+        }
+    
+        return back()->with('loginError', 'Masukkan Email & Password Dengan Benar');
+    }
+    
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
